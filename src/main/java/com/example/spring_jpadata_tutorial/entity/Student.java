02c9d797051c438025d0cbb63a3bd19c6df16b68
Victor Sanchez Grande <victorsg.dev@gmail.com>
2022-07-13 16:38:39 +0200

    JPA Annotations

--- src/main/java/com/example/spring_jpadata_tutorial/entity/Student.java
+++ src/main/java/com/example/spring_jpadata_tutorial/entity/Student.java
@@ -5,20 +5,40 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Table(
+        name = "tbl_student",
+        uniqueConstraints = @UniqueConstraint(
+                name = "emailid_unique",
+                columnNames = "email_address"
+        )
+)
 public class Student {
 
     @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long studentId;
     private String firstName;
     private String lastName;
+
+    @Column(
+            name = "email_address",
+            nullable = false
+    )
     private String emailId;
     private String guardianName;
     private String guardianEmail;
